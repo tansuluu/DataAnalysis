39f4a79d96284832da858cd542e70c0a23e9152f
tansuluu25 <tansuluu.myrzaeva@iaau.edu.kg>
2018-11-16 19:11:07 +0600

    Instructions: You are given a sorted sequence of n integers S = s1, s2, ..., sn and a sorted sequence of m integers Q = q1, q2, ..., qm. Please, print in the ascending order all such si that belongs to Q. Example Input: 5 -2 -1 0 1 4 6 -3 -2 -1 1 2 3 Output: -2 -1 1

---
+++ act2.py
@@ -0,0 +1,7 @@
+num=int(input())
+list1=list(map(int, input().strip().split()))
+num2=int(input())
+list2=list(map(int,input().strip().split()))
+for i in range(0,num):
+    if list1[i] in list2:
+        print(list1[i])
